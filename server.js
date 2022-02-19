34d6b7c17cc1f902fdd6c8093c96071a2e5e2546
vlad-lab319 <solo15mag@gmail.com>
2022-02-19 18:28:26 -0500

    Switch off dotenv and nodemon

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
   })
 );
 
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = 8080;
 
 const usersRoutes = require("./routes/users");
 const roomsRoutes = require("./routes/rooms");
@@ -34,7 +34,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3002",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
